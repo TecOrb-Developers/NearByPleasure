81084620fc4b131fca79cd892b7de3a050d11d09
priyadarshini <priyadarshanimini@gmail.com>
2016-05-25 19:32:07 +0530

    initial commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,4 +9,11 @@ class ReviewsController < ApplicationController
 			render :json => { :response_code => 500, :response_message => "something wrong"}
 		end
 	end
+
+	def my_review
+		@user=User.find_by_id(params[:user_id])
+		if @user.present?
+			@subcat=Subcategory.find_by_id(params[:subcategory_id])
+			
+	end
 end
