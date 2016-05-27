60e61db8eb9d74ccaf0166c78b9c8c2a11c2e24c
priyadarshini <priyadarshanimini@gmail.com>
2016-05-27 07:40:02 +0530

    initially

--- app/controllers/recent_checks_controller.rb
+++ app/controllers/recent_checks_controller.rb
@@ -1,17 +1,6 @@
 class RecentChecksController < ApplicationController
 
 
-	def create_recent
-		@user=User.find_by_id(params[:user_id])
-		if @user.present?
-			@subcat=Subcategory.find_by_id(params[:subcategory_id])
-			@recentchk=RecentCheck.create(:user_id=>@user.id,:subcategory_id=>@subcat.id)
-			render :json => { :response_code => 200, :response_message => "Recens are!!",:recent=>@recentchk.as_json(except: [:created_at,:updated_at]) }
-		else
-			render :json => { :response_code => 500, :response_message => "User not exist!!"}
-		end
-  end
-
 	def recent_service
 		@user=User.find_by_id(params[:user_id])
 		if @user.present?
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   post 'search_title'=>'titles#search_title'
   post 'recent_service'=>"recent_checks#recent_service"
   post 'all_recent'=>"recent_checks#all_recent"
-  post 'create_recent'=>"recent_checks#create_recent"
+ 
 
   post 'socialauth'=>"users#socialauth"
 
