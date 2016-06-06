0fd18a87150a4198609a726e70a22d51d4923b89
Jai <loveyhtu21@gmail.com>
2016-06-06 16:58:25 +0530

    make it better

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Category do
 
  permit_params :name, :image
+   filter :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
