6c02f062fdc1ea2628d4a792fd4dee584853e2e2
priyadarshini <priyadarshanimini@gmail.com>
2016-06-14 20:52:43 +0530

    make it

--- app/admin/ticket.rb
+++ app/admin/ticket.rb
@@ -12,33 +12,35 @@
 #end
 
 ActiveAdmin.register Ticket do
+  actions :all, except: [:edit, :destroy]
  config.breadcrumb = false
 	index do
     selectable_column
     id_column
     column :title
     column :user_id
-     #column "Business" do |resources|
-       #resources.Subcategory.title
-    #end
-
-    actions
+     column "Business" do |resources|
+       resources.subcategory.present? ? resources.subcategory.title : "Not Available"
+     end
+    actions 
   end
   filter :title
-  show do
-  	attributes_table do
-      row " " do |resources|
-        render :partial => 'tickets/show',:locals=>{:@ticket=>resources,:@comments=>resources.ticket_comments,:@subcategory=> resources.subcategory}
-        render :partial => 'ticket_comments/new',:locals=>{:@ticket=>resources,:@comments=>resources.ticket_comments}
-       
-      end
-    end
-  end
   controller do
     def show
       @ticket=Ticket.find_by_id(params[:id])
       @comments=@ticket.ticket_comments.order(created_at: "DESC")
     end
   end
-end
+  show do
+  	attributes_table do
+      row " " do |resources|
+        #render :partial => 'tickets/show',:locals=>{:@ticket=>resources,:@subcategory=> resources.subcategory}
+        render :partial => 'ticket_comments/new',:locals=>{:@ticket=>resources,:@subcategory=> resources.subcategory}
+        render :partial => 'tickets/show',:locals=>{:@ticket=>resources}  
 
+      end
+    end
+  end
+end
+#:@comments=>resources.ticket_comments
+#:@comments=>resources.ticket_comments
